25dc75ec93fc6be81ca404d76d1719fa7b3dc28d
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-04-23 15:37:47 +0530

    23-04-23

--- src/Pages/Sdp.jsx
+++ src/Pages/Sdp.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Scroll from '../Components/Scroll'
 import '../assessts/courses.css'
-import iic from '../images/iic.png'
+import sdp from '../images/sdp.png'
 export default function Sdp() {
   return (
     <div className='nptel'>
       <div className="div1">
-      <h1>Institution Innovation Council</h1>
+      <h1>ACE One Day Skill Development Program</h1>
       <div className="aboutNptel">
       <ul>
         <li>ACE One Day Skill Development Program is a unique initiative organized by Ambala College of Engineering.</li> 
-        <li>The program aims to provide school students with exposure to new technologies and skills in a fun and interactive way.</li> 
-        <li>Through this program, students can learn about topics such as robotics, coding, and 3D printing from experts in the field.</li> 
+        <li><b>The program aims to provide school students with exposure to new technologies and skills.</b></li> 
+        <li>Students learn about tech <b> such as robotics, coding, and 3D printing from experts in the field.</b></li> 
         <li>The program includes hands-on activities and challenges designed to encourage students to apply what they have learned.</li> 
-        <li>Participants in the ACE One Day Skill Development Program will have the opportunity to Interact with faculty and students for career insights.</li> 
+        <li>Participants will have the opportunity to Interact with faculty and students for career insights.</li> 
         <li>The program is free of charge, and ACE provides all the necessary equipment and materials for the workshops.</li> 
         <li>If you are interested in learning more about the ACE One Day Skill Development Program, please contact us for more information.</li> 
       </ul>
-      <img src={iic} alt="" />
+      <img src={sdp} alt="" />
       </div>
       </div>
       <div className="mission-vision">
@@ -39,9 +39,6 @@ export default function Sdp() {
 <li>Provide a platform for showcasing talents.</li>
 <li>Encourage social responsibility through technology use.</li></ol>
         </div>
-      </div>
-      <div className="contactNptel">
-        <Link to="https://www.ambalacollege.com/wp-content/uploads/2022/11/EstablismentCertificate.pdf"><button>Click here to view the Establishment Certificate</button></Link>
       </div>
       <Scroll/>
     </div>
