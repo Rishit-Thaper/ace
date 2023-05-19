6024d2572814e76dca8676f4324b3b9690f67826
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-05-19 11:17:04 +0530

    19-05-23

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assessts/blog.css'
-
+import Scroll from './Scroll';
 const Blogs = ({blogs}) => {
 
     console.log("Blog Object")
@@ -10,17 +10,17 @@ const Blogs = ({blogs}) => {
 
     return (
       <div className="card-list">
+        <Scroll/>
         {blogs.map((blog) => (
-        <div className="blog-card">
-  
-          <Link key={blog.id} to={`/blogs/${blog.id}`} className="card">
+
+          <div key={blog.id} to={`/blogs/${blog.id}`} className="blog-card">
             <img src={`http://localhost:1337${blog.attributes.blogMedia.data[0].attributes.url}`} alt={blog.attributes.blogtitle} />
             <div className="card-body">
               <h2>{blog.attributes.blogTitle}</h2>
               <p>{blog.attributes.blogDescription}</p>
+          <Link key={blog.id} to={`/blogs/${blog.id}`} className="card"><button>Read More</button></Link>
             </div>
-          </Link>
-        </div>
+          </div>
   
         ))}
   
--- src/Pages/BlogContent.jsx
+++ src/Pages/BlogContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import Scroll from '../Components/Scroll';
+import '../assessts/blog.css';
 const BlogContent = ({ blogs }) => {
   const { id } = useParams();
 
@@ -11,7 +12,9 @@ const BlogContent = ({ blogs }) => {
   }
 
   return (
+    
     <div>
+      <Scroll/>
       <img src={`http://localhost:1337${blog.attributes.blogMedia.data[0].attributes.url}`} />
       <h1>{blog.attributes.blogTitle}</h1>
       <div>{blog.attributes.blogContent}</div>
